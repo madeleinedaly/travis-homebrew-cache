6b676ec1e1162fbc1248c387c96bcfd61245015f
Madeleine Daly <mdaly@dailybreak.com>
2018-07-03 22:33:14 -0400

    try building only when HEAD has changed

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-echo "Checking if extglob is set..."
-getopt extglob
+INSTALL_RECEIPT="/usr/local/Cellar/emacs/(HEAD-*)/INSTALL_RECEIPT.json"
+
+LAST_COMMIT_BUILT=$(cat $INSTALL_RECEIPT | python -c "import sys, json; print(json.load(sys.stdin)['HEAD']))")
+CURRENT_UPSTREAM_HEAD=$(git ls-remote git://git.sv.gnu.org/emacs.git HEAD | awk '{print $1}')
 
 HOMEBREW_EMACS_INSTALLED="$(brew ls | grep -c emacs)"
 
-if ! [[ $HOMEBREW_EMACS_INSTALLED -eq 1 ]]; then
+echo 'Last commit built:' $LAST_COMMIT_BUILT
+echo 'Current upstream HEAD:' $CURRENT_UPSTREAM_HEAD
+
+if ! [[ $LAST_COMMIT_BUILT == $CURRENT_UPSTREAM_HEAD]] || ! [[ $HOMEBREW_EMACS_INSTALLED -eq 1 ]]; then
   echo 'Rebuilding Emacs...'
-  brew update
-  brew install emacs --with-cocoa --with-gnutls
+  brew install emacs --HEAD --with-cocoa --with-gnutls
 fi
