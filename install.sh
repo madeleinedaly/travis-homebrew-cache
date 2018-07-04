beebafd133eaebcb9765b0a7f9894d2569362a31
Madeleine Daly <mdaly@dailybreak.com>
2018-07-03 22:42:31 -0400

    fix script errors

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-INSTALL_RECEIPT="/usr/local/Cellar/emacs/(HEAD-*)/INSTALL_RECEIPT.json"
-
-LAST_COMMIT_BUILT=$(cat $INSTALL_RECEIPT | python -c "import sys, json; print(json.load(sys.stdin)['HEAD']))")
+LAST_COMMIT_BUILT=$(cat /usr/local/Cellar/emacs/(HEAD-*)/INSTALL_RECEIPT.json | python -c "import sys, json; print(json.load(sys.stdin)['HEAD'])")
 CURRENT_UPSTREAM_HEAD=$(git ls-remote git://git.sv.gnu.org/emacs.git HEAD | awk '{print $1}')
 
 HOMEBREW_EMACS_INSTALLED="$(brew ls | grep -c emacs)"
@@ -10,7 +8,7 @@ HOMEBREW_EMACS_INSTALLED="$(brew ls | grep -c emacs)"
 echo 'Last commit built:' $LAST_COMMIT_BUILT
 echo 'Current upstream HEAD:' $CURRENT_UPSTREAM_HEAD
 
-if ! [[ $LAST_COMMIT_BUILT == $CURRENT_UPSTREAM_HEAD]] || ! [[ $HOMEBREW_EMACS_INSTALLED -eq 1 ]]; then
+if [[ $LAST_COMMIT_BUILT != $CURRENT_UPSTREAM_HEAD ]] || ! [[ $HOMEBREW_EMACS_INSTALLED -eq 1 ]]; then
   echo 'Rebuilding Emacs...'
   brew install emacs --HEAD --with-cocoa --with-gnutls
 fi
