6ac720fdf293470abe15da62a0b2618f0c1e9830
Madeleine Daly <mdaly@dailybreak.com>
2018-07-04 14:50:36 -0400

    fix bad substitution

--- install.sh
+++ install.sh
@@ -4,12 +4,13 @@ HOMEBREW_EMACS_INSTALLED="$(brew ls | grep -c emacs)"
 INSTALL_DIR=$(find /usr/local/Cellar/emacs -type d -name 'HEAD-*')
 
 LAST_COMMIT_BUILT=$(echo ${INSTALL_DIR:29:7})
-CURRENT_UPSTREAM_HEAD=${"$(git ls-remote https://github.com/emacs-mirror/emacs.git HEAD | awk '{print $1}')":0:7}
+CURRENT_HEAD_LONG=$(git ls-remote https://github.com/emacs-mirror/emacs.git HEAD | awk '{print $1}')
+CURRENT_HEAD_SHORT=${CURRENT_HEAD_LONG:0:7}
 
 echo 'Last commit built:' $LAST_COMMIT_BUILT
 echo 'Current upstream HEAD:' $CURRENT_UPSTREAM_HEAD
 
-if [[ $CURRENT_UPSTREAM_HEAD != $LAST_COMMIT_BUILT ]] || ! [[ $HOMEBREW_EMACS_INSTALLED -eq 1 ]]; then
+if [[ $CURRENT_HEAD_SHORT != $LAST_COMMIT_BUILT ]] || ! [[ $HOMEBREW_EMACS_INSTALLED -eq 1 ]]; then
   echo 'Rebuilding Emacs...'
   brew uninstall emacs
   brew update
