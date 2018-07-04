1915074802b4a39d0d2eb1b95a1a042e9b72d9b5
Madeleine Daly <mdaly@dailybreak.com>
2018-07-03 22:17:59 -0400

    print extglob

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Checking if extglob is set..."
+getopt extglob
+
 HOMEBREW_EMACS_INSTALLED="$(brew ls | grep -c emacs)"
 
 if ! [[ $HOMEBREW_EMACS_INSTALLED -eq 1 ]]; then
