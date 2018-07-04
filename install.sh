d4e9232e3949b9223b1041c02d07be2d9632b432
Madeleine Daly <mdaly@dailybreak.com>
2018-07-04 14:33:40 -0400

    fix HEAD sha parsing

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
+HOMEBREW_EMACS_INSTALLED="$(brew ls | grep -c emacs)"
 INSTALL_DIR=$(find /usr/local/Cellar/emacs -type d -name 'HEAD-*')
 
-LAST_COMMIT_BUILT=$(cat "$INSTALL_DIR/INSTALL_RECEIPT.json" | python -c "import sys, json; print(json.load(sys.stdin)['HEAD'])")
+LAST_COMMIT_BUILT=$(echo ${INSTALL_DIR:29:7})
 CURRENT_UPSTREAM_HEAD=$(git ls-remote git@github.com:emacs-mirror/emacs.git HEAD | awk '{print $1}')
-
-HOMEBREW_EMACS_INSTALLED="$(brew ls | grep -c emacs)"
 
 echo 'Last commit built:' $LAST_COMMIT_BUILT
 echo 'Current upstream HEAD:' $CURRENT_UPSTREAM_HEAD
