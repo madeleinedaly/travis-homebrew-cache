cf2b897d1f076c7b5e1a7994a74621b6bc5660d1
Madeleine Daly <mdaly@dailybreak.com>
2018-07-04 14:45:24 -0400

    get short sha from upstream

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ HOMEBREW_EMACS_INSTALLED="$(brew ls | grep -c emacs)"
 INSTALL_DIR=$(find /usr/local/Cellar/emacs -type d -name 'HEAD-*')
 
 LAST_COMMIT_BUILT=$(echo ${INSTALL_DIR:29:7})
-CURRENT_UPSTREAM_HEAD=$(git ls-remote https://github.com/emacs-mirror/emacs.git HEAD | awk '{print $1}')
+CURRENT_UPSTREAM_HEAD=${"$(git ls-remote https://github.com/emacs-mirror/emacs.git HEAD | awk '{print $1}')":0:7}
 
 echo 'Last commit built:' $LAST_COMMIT_BUILT
 echo 'Current upstream HEAD:' $CURRENT_UPSTREAM_HEAD
