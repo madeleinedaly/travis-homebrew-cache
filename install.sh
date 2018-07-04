2461f1ef53f702ed4ee81a94a7fcf2f5683d7278
Madeleine Daly <mdaly@dailybreak.com>
2018-07-04 14:03:31 -0400

    fix head url

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 INSTALL_DIR=$(find /usr/local/Cellar/emacs -type d -name 'HEAD-*')
 
 LAST_COMMIT_BUILT=$(cat "$INSTALL_DIR/INSTALL_RECEIPT.json" | python -c "import sys, json; print(json.load(sys.stdin)['HEAD'])")
-CURRENT_UPSTREAM_HEAD=$(git ls-remote git://git.sv.gnu.org/emacs.git HEAD | awk '{print $1}')
+CURRENT_UPSTREAM_HEAD=$(git ls-remote git@github.com:emacs-mirror/emacs.git HEAD | awk '{print $1}')
 
 HOMEBREW_EMACS_INSTALLED="$(brew ls | grep -c emacs)"
 
